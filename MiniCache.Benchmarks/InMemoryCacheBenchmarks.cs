882c0d6add418bc30b7b11c8d03a3eea85c0ac6e
Nik Lebn <nlebn001@gmail.com>
2025-12-07 17:17:12 +0100

    benchmarks adjustment

--- MiniCache.Benchmarks/InMemoryCacheBenchmarks.cs
+++ MiniCache.Benchmarks/InMemoryCacheBenchmarks.cs
@@ -5,14 +5,16 @@ using MiniCache.Core;
 namespace MiniCache.Benchmarks;
 
 
+
+[SimpleJob(RuntimeMoniker.HostProcess, launchCount: 1, warmupCount: 1, iterationCount: 5)]
 [MemoryDiagnoser]
 public class InMemoryCacheBenchmarks
 {
     private ICache _cache = null!;
     private ReadOnlyMemory<byte> _payload = Array.Empty<byte>();
 
-    [Params(1, 100, 1000)]
-
+    //[Params(1, 100, 1000)] // keep all for precise numbers
+    [Params(1000)] // keep just 1 for quick tuning
     public int InitialEntries { get; set; }
 
     [GlobalSetup]
@@ -33,32 +35,11 @@ public class InMemoryCacheBenchmarks
     public void Cleanup()
     {
         if (_cache is IDisposable disposable)
-        {
             disposable.Dispose();
-        }
     }
 
-    [Benchmark]
-    public void SetSingle()
-    {
-        _cache.Set("bench-single", _payload, TimeSpan.FromMinutes(5));
-    }
-
-    [Benchmark]
-    public void TryGetExisting()
-    {
-        _cache.TryGet("bench-0", out _);
-    }
-
-    [Benchmark]
-    public void TryGetMissing()
-    {
-        _cache.TryGet("does-not-exist", out _);
-    }
-
-    [Benchmark]
-    public void MetadataAccess()
-    {
-        _cache.TryGetMetadata("bench-0", out _);
-    }
+    [Benchmark] public void SetSingle() => _cache.Set("bench-single", _payload, TimeSpan.FromMinutes(5));
+    [Benchmark] public void TryGetExisting() => _cache.TryGet("bench-0", out _);
+    [Benchmark] public void TryGetMissing() => _cache.TryGet("does-not-exist", out _);
+    [Benchmark] public void MetadataAccess() => _cache.TryGetMetadata("bench-0", out _);
 }
